f80c17ce63935f8daa8512761062ad433c6c912b
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2019-12-19 21:56:24 -0500

    add control of lifter done with class at today's session

--- rover/rover.m
+++ rover/rover.m
@@ -2,15 +2,15 @@
 % https://www.mathworks.com/matlabcentral/answers/335596-how-to-make-matlab-detect-keyboard-stroke
 global r
 global v
-% global w
+global u
 
 r = true;
 v = tcpip('192.168.43.192',25000);
 v.ByteOrder = 'littleEndian';
 fopen(v);
-% w = tcpip('192.168.43.192',25001);
-% w.ByteOrder = 'littleEndian';
-% fopen(w);
+u = tcpip('192.168.43.192',25001);
+u.ByteOrder = 'littleEndian';
+fopen(u);
 
 h = figure;
 set(h,'KeyPressFcn',@move);
@@ -20,11 +20,12 @@ while r
 end
 close(h);
 fclose(v);
-% fclose(w);
+fclose(u);
 
 function move(~,e)
     global r
     global v
+    global u
     fprintf('key pressed: %s\n',e.Key);
     if strcmpi(e.Key,'control')
         r = false;
@@ -38,9 +39,11 @@ function move(~,e)
             % This loop runs for approximately 1 second
             fwrite(v,-1,'double');
         end
-    elseif strcmpi(e.Key,'rightarrow') || strcmpi(e.Key,'d')
-        r = true;
     elseif strcmpi(e.Key,'leftarrow') || strcmpi(e.Key,'a')
         r = true;
+    elseif strcmpi(e.Key,'space')
+        for angle = 125:-1:0
+            fwrite(u,angle,'double');
+        end
     end
 end
